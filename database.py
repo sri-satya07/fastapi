453856c4943b847e5dbabbbbcaa3f562152e4f4d
Sri Satyarama <146422054+srisatyarama@users.noreply.github.com>
2024-10-29 13:30:47 +0530

    abc

--- database.py
+++ database.py
@@ -1,39 +1,10 @@
-import pymysql
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+
 from sqlalchemy.orm import sessionmaker
 
-# Using pymysql to create a connection
-conn = pymysql.connect(db='fastapi', user='root', passwd='Satya_123', host='localhost')
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Satya_123@localhost/fastapi"
 
-# Get a SQLAlchemy engine using the pymysql connection
-engine = create_engine("mysql+pymysql://", creator=lambda: conn)
-
-# Create a session local factory
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Declare a base class for declarative models
 Base = declarative_base()
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import declarative_base
-
-# from sqlalchemy.orm import sessionmaker
-
-# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Satya_123@localhost/fastapi"
-
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# Base = declarative_base()
