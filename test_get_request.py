99de09306298abf5d5f5b99b5839dba8e00b2a99
Sri Satyarama <146422054+srisatyarama@users.noreply.github.com>
2024-11-19 12:02:03 +0530

    Written testcases for get user and create uses

--- test_get_request.py
+++ test_get_request.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
-from typing import List
-from main import app  # Import your FastAPI app
+from main import app  
+from faker import Faker
 
 client = TestClient(app)
 @pytest.mark.order(1)
@@ -9,3 +9,24 @@ def test_get_users():
     response = client.get("/api/hello/")
     assert response.status_code == 200     
     print("helloowwwww")
+
+@pytest.mark.order(2)  
+def test_get_user():
+    response = client.get("/api/users/")
+    assert response.status_code == 200
+    #get users from the response
+    users = response.json()
+    print(users)
+
+
+@pytest.mark.order(3)
+def test_create_users():
+    body = {
+    "name": "sai",
+    "email": "sai@gmail.com",
+    "password": "sai@123"
+}
+   
+    response = client.post("/api/users/create/", json=body)
+    assert response.status_code == 200
+    
